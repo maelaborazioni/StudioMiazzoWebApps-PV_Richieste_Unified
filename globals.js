c53cd68f0f3d2e868c1142382eb1fff76761e42b
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2019-09-10 18:06:52 +0200

    [ADD] Acquisizione tracciato Welfare esterno

--- globals.js
+++ globals.js
@@ -35,6 +35,21 @@ function openRequestSelection()
 }
 
 /**
+ * @properties={typeid:24,uuid:"92F9220E-AD16-4FB8-9693-AE0526D9658C"}
+ */
+function openRequestTrackSelection()
+{
+	if(globals.ma_utl_hasKeySede())
+	{
+		globals.ma_utl_showErrorDialog('i18n:ma.msg.not_available');
+		return;
+	}
+	
+	var frm = forms.pvd_selezione_welfare;
+	globals.ma_utl_showFormInDialog(frm.controller.getName(),'Seleziona i parametri per l\'importazione');
+}
+
+/**
  * @properties={typeid:24,uuid:"8378682D-B2EB-402C-8AE0-0145F221D514"}
  */
 function openRequest()
@@ -120,4 +135,215 @@ function onActionBtnDettaglioGiorni(event)
 	
 	globals.ma_utl_showFormDialog({ name: detailForm.name, title: 'Dettaglio variazione', blocking: true});
 	
+}
+
+/**
+ * TODO generated, please specify type and doc for the params
+ * @param {Array<Number>} arrLavoratoriRichieste
+ * 
+ * @return {RuntimeForm<>}
+ * 
+ * @properties={typeid:24,uuid:"17371ADE-74CD-4CF5-823C-308D2E114609"}
+ */
+function costruisciRiepilogoAnomalieRichieste(arrLavoratoriRichieste)
+{
+	var frm = forms.pvl_welfare_richieste_tbl;
+	var frmName = frm.controller.getName();
+	var frmNameTemp = frmName + '_temp';
+
+	var frmTab = forms.pvl_welfare_richieste_tab;
+	
+	frmTab.elements.tab_welfare.removeAllTabs();
+	
+    if(forms[frmNameTemp] != null)
+    {
+		history.removeForm(frmNameTemp);
+        solutionModel.removeForm(frmNameTemp);
+    }
+    
+    solutionModel.cloneForm(frmNameTemp,solutionModel.getForm(frmName));
+    
+    var cols = ['id','codice','nominativo','tipo_richiesta','quantita','base','importo','escludi'];
+    
+    var types = [JSColumn.NUMBER,JSColumn.NUMBER,JSColumn.TEXT,JSColumn.TEXT,JSColumn.NUMBER,JSColumn.NUMBER,JSColumn.NUMBER,JSColumn.NUMBER];
+
+    var ds = databaseManager.createEmptyDataSet(0,cols);
+    
+    for(var i = 1; i <= arrLavoratoriRichieste.length; i++)
+    {
+    	if(arrLavoratoriRichieste[i])
+    	{
+    		var obj = getInfoFromWelfareRichiesta(arrLavoratoriRichieste[i])
+    		var currRow = [obj['id'],obj['codice'],obj['nominativo'],obj['tipo_richiesta'],obj['quantita'],obj['base'],obj['importo'],0];
+    	
+    		ds.addRow(currRow);
+    	}
+    }
+    
+    var dS = ds.createDataSource('dS_' + application.getUUID(),types);
+    solutionModel.getForm(frmNameTemp).dataSource = dS;
+    solutionModel.getForm(frmNameTemp).getField('fld_id').dataProviderID = 'id';
+    solutionModel.getForm(frmNameTemp).getField('fld_codice').dataProviderID = 'codice';
+    solutionModel.getForm(frmNameTemp).getField('fld_nominativo').dataProviderID = 'nominativo';
+    solutionModel.getForm(frmNameTemp).getField('fld_tipo_richiesta').dataProviderID = 'tipo_richiesta';
+    solutionModel.getForm(frmNameTemp).getField('fld_quantita').dataProviderID = 'quantita';
+    solutionModel.getForm(frmNameTemp).getField('fld_base').dataProviderID = 'base';
+    solutionModel.getForm(frmNameTemp).getField('fld_importo').dataProviderID = 'importo';
+    solutionModel.getForm(frmNameTemp).getField('fld_escludi').dataProviderID = 'escludi';
+    
+    frmTab.elements.tab_welfare.addTab(frmNameTemp);
+    
+    return frmTab;
+}
+
+
+/**
+ * @AllowToRunInFind
+ * 
+ * TODO generated, please specify type and doc for the params
+ * @param {Number} idLavoratoreRichiesta
+ * 
+ * 
+ * @properties={typeid:24,uuid:"1B08B007-F685-4BBC-B27C-84D72DA0C159"}
+ */
+function getInfoFromRichiesta(idLavoratoreRichiesta)
+{
+	/** @type {JSFoundSet<db:/ma_richieste/lavoratori_richieste>} */
+	var fs = databaseManager.getFoundSet(globals.Server.MA_RICHIESTE,globals.Table.LAVORATORI_RICHIESTE);
+	if(fs.find())
+	{
+		fs.idlavoratorerichiesta = idLavoratoreRichiesta;
+		
+		if(fs.search())
+		{
+			var idLavoratore = globals.ma_utl_lav_toCliente(fs.idlavoratore);
+			var tipoRichiesta = fs.lavoratori_richieste_to_tab_richiestedettaglio.codice + ' - ' + fs.lavoratori_richieste_to_tab_richiestedettaglio.descrizione;
+			var importo = getImportoFromRichiesta(idLavoratoreRichiesta);
+			
+			return {
+				codice : globals.getCodLavoratore(idLavoratore),
+				nominativo : globals.getNominativo(idLavoratore),
+				tipo_richiesta : tipoRichiesta,
+				valore_richiesta : importo
+			}
+		}
+	}
+	
+	return null;
+}
+
+/**
+ * @AllowToRunInFind
+ * 
+ * TODO generated, please specify type and doc for the params
+ * @param {Number} idWelfareLavoratoreRichiesta
+ *
+ * @properties={typeid:24,uuid:"A91A902E-6522-4B60-B6E0-0C73EC31A170"}
+ */
+function getInfoFromWelfareRichiesta(idWelfareLavoratoreRichiesta)
+{
+	/** @type {JSFoundSet<db:/ma_richieste/welfare_lavoratori_richieste>} */
+	var fs = databaseManager.getFoundSet(globals.Server.MA_RICHIESTE,'welfare_lavoratori_richieste');
+	if(fs.find())
+	{
+		fs.idwelfarelavoratorerichiesta = idWelfareLavoratoreRichiesta;
+		if(fs.search())
+		{
+			var idLavoratore = globals.ma_utl_lav_toCliente(fs.welfare_lavoratori_richieste_to_lavoratori_richieste.idlavoratore);
+						
+			var obj = {
+				id : idWelfareLavoratoreRichiesta,
+				codice : globals.getCodLavoratore(idLavoratore),
+				nominativo : globals.getNominativo(idLavoratore),
+				quantita : fs.quantita,
+				base : fs.base,
+				importo : fs.importo,
+				tipo_richiesta : getDescrizioneTracciatoConversione(fs.welfare_lavoratori_richieste_to_welfare_ditte_richieste.idtabwelfaredittatracciato
+					                                                ,fs.welfare_lavoratori_richieste_to_welfare_ditte_richieste.idtabwelfaretipopiano
+																	,fs.codicetracciato
+																	,fs.codicetracciatodettaglio)
+			}
+			
+			return obj;
+		}
+	}
+	
+	return null;
+}
+
+/**
+ * @AllowToRunInFind
+ * 
+ * TODO generated, please specify type and doc for the params
+ * @param {Number} idTabWelfareDittaTracciato
+ * @param {Number} idTabWelfareTipoPiano
+ * @param {String} codiceTracciato
+ * @param {String} codiceTracciatoDettaglio
+ *
+ * @properties={typeid:24,uuid:"97C39058-ADDC-4512-960B-B5C01EBDAA1A"}
+ */
+function getDescrizioneTracciatoConversione(idTabWelfareDittaTracciato,idTabWelfareTipoPiano,codiceTracciato,codiceTracciatoDettaglio)
+{
+	/** @type {JSFoundSet<db:/ma_richieste/tab_welfare_dittetracciati_conversione>} */
+	var fs = databaseManager.getFoundSet(globals.Server.MA_RICHIESTE,'tab_welfare_dittetracciati_conversione');
+	if(fs.find())
+	{
+		fs.idtabwelfaredittatracciato = idTabWelfareDittaTracciato;
+		fs.idtabwelfaretipopiano = idTabWelfareTipoPiano;
+		fs.codicetracciato = codiceTracciato;
+		fs.codicetracciatodettaglio = codiceTracciatoDettaglio;
+		
+		if(fs.search())
+			return fs.descrizionetracciato;
+	}
+	
+	return '';
+}
+
+
+/**
+ * @AllowToRunInFind
+ * 
+ * TODO generated, please specify type and doc for the params
+ * @param idLavoratoreRichiesta
+ *
+ * @properties={typeid:24,uuid:"C22447C6-BEEA-4079-B779-910912344BE3"}
+ */
+function getImportoFromRichiesta(idLavoratoreRichiesta)
+{
+	/** @type {JSFoundSet<db:/ma_richieste/lavoratori_richiestecampi>} */
+	var fs = databaseManager.getFoundSet(globals.Server.MA_RICHIESTE,'lavoratori_richiestecampi');
+	if(fs.find())
+	{
+		fs.idlavoratorerichiesta = idLavoratoreRichiesta;
+		fs.codice = 'IMP';
+	
+		if(fs.search())
+			return fs.valore;
+	}
+	
+	return null;
+}
+
+/**
+ * @AllowToRunInFind
+ * 
+ * Elimina il record in tabella relativo al file temporaneo caricato
+ * 
+ * @param {String} fileId
+ *
+ * @properties={typeid:24,uuid:"6025C5D3-1848-43CE-8B1C-FFF050643B33"}
+ */
+function eliminaTemporaryFile(fileId)
+{
+	/** @type {JSFoundSet<db:/ma_richieste/ditte_temporaryfile>} */
+	var fs = databaseManager.getFoundSet(globals.Server.MA_RICHIESTE,'ditte_temporaryfile');
+	if(fs.find())
+	{
+		fs.id = fileId;
+		if(fs.search())
+			return fs.deleteRecord(fs.getSelectedRecord());
+	}
+	
+	return false;
 }
